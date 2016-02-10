16e3a33d7d038142291d3d68ce74c9573f8b2949
hounenhounen <kawahara@kawahara-no-MacBook-Air.local>
2016-02-10 16:24:53 +0900

    Commit1

---
+++ Assets/Scripts/SaveScore.cs
@@ -0,0 +1,15 @@
+﻿using NCMB; //mobile backendのSDKを読み込む
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SaveScore : MonoBehaviour {
+	// mobile backendに接続------------------------
+	public void save( string name, int score ) {
+		NCMBObject obj = new NCMBObject ("Score");
+		obj ["name"] = name;//オブジェクトに名前とスコアを設定
+		obj ["score"] = score;
+		obj ["Log"] = Player.posList;
+		obj.SaveAsync ();//この処理でサーバーに書き込む
+	}
+}
